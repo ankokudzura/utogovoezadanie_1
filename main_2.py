ed1edd4a31ef91878ce00881c3b7d6fea54c8b01
ankokudzura <75267061+ankokudzura@users.noreply.github.com>
2020-12-24 01:36:57 +0300

    Add files via upload

---
+++ main_2.py
@@ -0,0 +1,31 @@
+import re
+def filling_list(tmp_list):
+    new_variable=''
+    while True:
+        new_variable = input("Please enter veriable, or press enter to finish: ")
+        if new_variable == '':
+            return tmp_list
+        tmp_list.append(new_variable)
+def solution (list1):
+    for i in list1:
+        counter = [0, 0, 0, 0, 0]
+        for j in i:
+            if re.search("[`1234567098\-=]", j): counter[0] += 1
+            if re.search("[`qwertyuiop\[\]]", j): counter[1] += 1
+            if re.search("[asdfghjklASDFGHJKL;']", j): counter[2] += 1
+            if re.search("[zxcvbnm,\.\/ZXCVBNM<>\?]", j): counter[3] += 1
+            if (j == " "): counter[4] += 1
+        cout = 0
+        for k in counter:
+            if k > 0:
+                cout += 1
+        if cout > 1:
+            list1.remove(i)
+    print(list1)
+
+
+
+print("Enter data for the  list")
+list1=[]
+filling_list(list1)
+solution(list1)
